ba69b7665a6cddb7806789e06e82f266d2caa411
Aleksandr <meln.alex97@gmail.com>
2023-11-14 01:09:35 +0700

    Add network ui logic

---
+++ Assets/Code/NetworkUI.cs
@@ -0,0 +1,38 @@
+using TMPro;
+using Unity.Netcode;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NetworkUI : NetworkBehaviour
+{
+    [SerializeField] private Button _hostButton;
+    [SerializeField] private Button _clientButton;
+    [SerializeField] private TextMeshProUGUI _connectedPlayersCount;
+
+    private NetworkVariable<int> _playersCount 
+        = new NetworkVariable<int>(0, NetworkVariableReadPermission.Everyone);
+
+    private void Awake()
+    {
+        _hostButton.onClick.AddListener(() =>
+        {
+            NetworkManager.Singleton.StartHost();
+        });
+
+        _clientButton.onClick.AddListener(() =>
+        {
+            NetworkManager.Singleton.StartClient();
+        });
+    }
+
+
+    private void Update()
+    {
+        if (IsServer) 
+        {
+            _playersCount.Value = NetworkManager.Singleton.ConnectedClients.Count;
+        }
+
+        _connectedPlayersCount.text = $"Players: {_playersCount.Value }";
+    }
+}
